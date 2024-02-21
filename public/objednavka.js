fec9a6a64491e4510a48169fbcfd399672fa52c8
Doky5 <142930877+Doky5@users.noreply.github.com>
2024-02-21 21:11:43 +0100

    Delete public directory

--- public/objednavka.js
+++
@@ -1,9 +0,0 @@
-const dialog = document.getElementById("dialog");
-const paragraph = document.getElementById("paragraph");
-const pojem = document.getElementById("produkt");
-
-function Objednávka(produkt) {
-    dialog.showModal()
-    pojem.value = produkt;
-    paragraph.innerHTML = produkt;
-}
