b54012eecf93df0bd3a03d1840fe5d266a19d98f
Doky5 <jdokulil5@gmail.com>
2024-02-24 10:26:46 +0100

    objednávka

--- public/objednavka.js
+++ public/objednavka.js
@@ -1,9 +1,11 @@
 const dialog = document.getElementById("dialog");
 const paragraph = document.getElementById("paragraph");
 const pojem = document.getElementById("produkt");
+let barva = document.getElementById("barva");
 
 function Objednávka(produkt) {
     dialog.showModal()
+    barva.value = currentColor
     pojem.value = produkt;
     paragraph.innerHTML = produkt;
 }
